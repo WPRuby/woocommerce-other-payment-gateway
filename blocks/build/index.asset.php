848ed6dd8b340ef6b1e354ad509ca4232111f8da
Waseem Senjer <waseem.senjer@gmail.com>
2024-11-24 21:50:07 +0100

    fix validation

---
+++ blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-html-entities'), 'version' => 'd464cdafcd6a551b8ebd');
--- class-woocommerce-other-payment-gateway.php
+++ class-woocommerce-other-payment-gateway.php
@@ -172,15 +172,21 @@ class WC_Other_Payment_Gateway extends WC_Payment_Gateway{
 	}
 
 	public function validate_fields() {
-	    if($this->text_box_required === 'no'){
+	    if ($this->text_box_required === 'no') {
 	        return true;
         }
 
+        if ($this->hide_text_box === 'yes') {
+            return true;
+        }
+
 	    $textbox_value = (isset($_POST['other_payment-admin-note']))? trim($_POST['other_payment-admin-note']): '';
-		if($textbox_value === ''){
+
+		if ($textbox_value === '') {
 			wc_add_notice( __('Please, complete the payment information.','woocommerce-custom-payment-gateway'), 'error');
 			return false;
         }
+
 		return true;
 	}
 
