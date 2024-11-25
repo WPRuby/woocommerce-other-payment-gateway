874157c4f8f7ce7de65e0badfa67b424212fbd61
Waseem Senjer <waseem.senjer@gmail.com>
2024-11-25 17:38:25 +0100

    build

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-html-entities'), 'version' => 'd464cdafcd6a551b8ebd');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-html-entities'), 'version' => '2c1dced72837db059dd2');
